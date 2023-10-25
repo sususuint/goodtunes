853ce75446df5b091561957af44575a7490f4f7d
sususuint <113937210+sususuint@users.noreply.github.com>
2023-10-25 11:06:28 -0400

    first commit
    based on potd 5

---
+++ friend-db.php
@@ -0,0 +1,58 @@
+<?php
+function addFriend($friendname, $major, $year) 
+{
+  global $db; 
+  // bad way
+  // $query = "insert into friends values ('" . $friendname . "', '" . $major . "'," . $year .") ";
+  // $db->query($query);  // compile + exe
+
+  // good way
+  $query = "insert into friends values (:friendname, :major, :year) ";
+  // prepare: 
+  // 1. prepare (compile) 
+  // 2. bindValue + exe
+
+  $statement = $db->prepare($query); 
+  $statement->bindValue(':friendname', $friendname);
+  $statement->bindValue(':major', $major);
+  $statement->bindValue(':year', $year);
+  $statement->execute();
+  $statement->closeCursor();
+}
+
+function getAllFriends()
+{
+  global $db;
+  $query = "select * from friends";
+  $statement = $db->prepare($query); 
+  $statement->execute();
+  $results = $statement->fetchAll();   // fetch()
+  $statement->closeCursor();
+  return $results;
+}
+
+function updateFriendByName($name, $major, $year)
+{
+  global $db;
+  $query = "update friends set major=:major, year=:year where name=:name";
+
+  $statement = $db->prepare($query); 
+  $statement->bindValue(':name', $name);
+  $statement->bindValue(':major', $major);
+  $statement->bindValue(':year', $year);
+  $statement->execute();
+  $statement->closeCursor();
+}
+
+function deleteFriend($name)
+{
+  global $db;
+  $query = "delete from friends where name=:name";
+
+  $statement = $db->prepare($query); 
+  $statement->bindValue(':name', $name);
+  $statement->execute();
+  $statement->closeCursor();
+}
+
+?>
